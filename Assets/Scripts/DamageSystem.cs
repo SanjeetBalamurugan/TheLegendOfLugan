f18f7567af870e274b6fce523ca786807553ca13
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-08-21 20:24:18 +0530

    Update DamageSystem.cs

--- Assets/Scripts/DamageSystem.cs
+++ Assets/Scripts/DamageSystem.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class DamageSystem : MonoBehaviour
 {
-    public int damage;
+    public int damage = 10;
     public float damageRadius = 2f;
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
 
     public void ApplyDamage()
     {
-        Collider[] hitEnemies = Physics.OverlapSphere(transform.position, damageRadius);
-
-        foreach (var enemy in hitEnemies)
+        Vector3 origin = attackPoint ? attackPoint.position : transform.position;
+        Collider[] hit = Physics.OverlapSphere(origin, damageRadius, enemyLayers);
+        for (int i = 0; i < hit.Length; i++)
         {
-            if (enemy.CompareTag("Enemy"))
-            {
-                enemy.GetComponent<EnemyHealth>().TakeDamage(damage);
-            }
+            var health = hit[i].GetComponent<EnemyHealth>();
+            if (health != null) health.TakeDamage(damage);
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 origin = attackPoint ? attackPoint.position : transform.position;
+        Gizmos.DrawWireSphere(origin, damageRadius);
     }
 }
