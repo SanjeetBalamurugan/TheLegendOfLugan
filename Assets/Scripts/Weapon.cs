9c268f09f7e6ddf02544e1103c09f3ffb1d60d73
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-08-21 19:35:06 +0530

    Create Weapon.cs

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public enum WeaponType
+{
+    Sword,
+    Claymore,
+    Bow,
+}
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapons/Weapon")]
+public class Weapon : ScriptableObject
+{
+    public WeaponType weaponType;
+    public string weaponName;
+    public int damage;
+    public float attackSpeed;
+    public float range;
+    public string attackAnimationTrigger;
+}
