1b6fa8f05fb27cf3667e7dea2c7629c2bd44592b
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-08-21 20:23:12 +0530

    Update IPlayerState.cs

--- Assets/Scripts/TPVPlayer/IPlayerState.cs
+++ Assets/Scripts/TPVPlayer/IPlayerState.cs
@@ -17,23 +17,22 @@ public class IdleState : IPlayerState
     {
         this.player = player;
         player.GetAnimator().SetBool("IsMoving", false);
-        Debug.Log("Idle");
     }
 
     public void Exit() {}
 
-    public void Update() {
+    public void Update()
+    {
         HandleInput();
     }
 
     public void HandleInput()
     {
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
-            Debug.Log("MoveStateChange");
             player.SetState(new MoveState());
         }
-        if (Input.GetButtonDown("Fire1"))
+        else if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2"))
         {
             player.SetState(new CombatState());
         }
@@ -43,48 +42,55 @@ public class IdleState : IPlayerState
 public class MoveState : IPlayerState
 {
     private PlayerStateManager player;
-    private float targetX = 0f;
-    private float targetY = 0f;
-    private float currentX = 0f;
-    private float currentY = 0f;
-    private float smoothing = 0.03f;
-    private float runningSmoothing = 0.03f;
-    private TPVPlayerMove _PlayerMove;
+    private TPVPlayerMove mover;
+    private float targetX;
+    private float targetY;
+    private float currentX;
+    private float currentY;
+    private float smoothing = 0.08f;
+    private float runningSmoothing = 0.08f;
+    private float verticalVelocity;
 
     public void Enter(PlayerStateManager player)
     {
         this.player = player;
-        player.animator.SetBool("IsMoving", true);
-        _PlayerMove = player.GetPlayerMove();
+        mover = player.GetPlayerMove();
+        player.GetAnimator().SetBool("IsMoving", true);
+        verticalVelocity = 0f;
     }
 
     public void Exit()
     {
-        player.animator.SetBool("IsMoving", false);
+        player.GetAnimator().SetBool("IsMoving", false);
+        player.GetAnimator().SetFloat("x", 0f);
+        player.GetAnimator().SetFloat("y", 0f);
     }
 
     public void Update()
     {
-        HandleMovementInput();
+        HandleMovement();
+        HandleInput();
         UpdateAnimator();
     }
 
     public void HandleInput()
     {
-        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2"))
+        {
+            player.SetState(new CombatState());
+            return;
+        }
+
+        if (Mathf.Approximately(Input.GetAxisRaw("Horizontal"), 0f) && Mathf.Approximately(Input.GetAxisRaw("Vertical"), 0f) && mover.IsGrounded() && Mathf.Abs(verticalVelocity) < 0.01f)
         {
             player.SetState(new IdleState());
         }
-        if (Input.GetButtonDown("Fire1"))
-        {
-            player.SetState(new CombatState());
-        }
     }
 
-    private void HandleMovementInput()
+    private void HandleMovement()
     {
         bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        float currentSmoothing = isRunning ? runningSmoothing : smoothing;
+        float lerp = isRunning ? runningSmoothing : smoothing;
 
         targetX = 0f;
         targetY = 0f;
@@ -95,85 +101,92 @@ public class MoveState : IPlayerState
         if (Input.GetKey(KeyCode.A)) targetX = isRunning ? -1f : -0.5f;
         else if (Input.GetKey(KeyCode.D)) targetX = isRunning ? 1f : 0.5f;
 
-        currentX = Mathf.Lerp(currentX, targetX, currentSmoothing);
-        currentY = Mathf.Lerp(currentY, targetY, currentSmoothing);
+        currentX = Mathf.Lerp(currentX, targetX, lerp);
+        currentY = Mathf.Lerp(currentY, targetY, lerp);
 
-        Vector3 forward = _PlayerMove.gameObject.transform.forward;
-        Vector3 right = _PlayerMove.gameObject.transform.right;
-        float jUP = 0;
-
-        if (Input.GetButtonDown("Jump") && _PlayerMove.IsGrounded())
+        if (mover.IsGrounded())
         {
-            jUP = Mathf.Sqrt(_PlayerMove.jumpHeight * -2f * _PlayerMove.GetGravity());
-        }
-
-        if (isRunning)
-        {
-            Vector3 moveDirection = forward * currentY + right * currentX + jUP * _PlayerMove.gameObject.transform.up;
-
-            if (currentY > 0)
+            if (verticalVelocity < 0f) verticalVelocity = -2f;
+            if (Input.GetButtonDown("Jump"))
             {
-                _PlayerMove.controller.Move(moveDirection * _PlayerMove.runSpeed * Time.deltaTime);
-                _PlayerMove.gameObject.transform.rotation = Quaternion.LookRotation(forward);
-            }
-            else
-            {
-                _PlayerMove.controller.Move(moveDirection * _PlayerMove.runSpeed * Time.deltaTime);
+                verticalVelocity = Mathf.Sqrt(mover.jumpHeight * -2f * mover.GetGravity());
             }
         }
-        else
+        verticalVelocity += mover.GetGravity() * Time.deltaTime;
+
+        Vector3 forward = mover.transform.forward;
+        Vector3 right = mover.transform.right;
+        Vector3 move = forward * currentY + right * currentX;
+        if (isRunning) move *= mover.runSpeed;
+        else move *= mover.moveSpeed;
+
+        move += Vector3.up * verticalVelocity;
+        mover.controller.Move(move * Time.deltaTime);
+
+        if (isRunning && currentY > 0.01f)
         {
-            Vector3 moveDirection = forward * currentY + right * currentX;
-            _PlayerMove.controller.Move(moveDirection * _PlayerMove.moveSpeed * Time.deltaTime);
+            mover.transform.rotation = Quaternion.LookRotation(forward);
+        }
+        else if (move.sqrMagnitude > 0.0001f)
+        {
+            Vector3 flat = new Vector3(move.x, 0f, move.z);
+            if (flat.sqrMagnitude > 0.0001f)
+            {
+                mover.transform.rotation = Quaternion.LookRotation(flat);
+            }
         }
     }
 
     private void UpdateAnimator()
     {
-        player.animator.SetFloat("x", currentX);
-        player.animator.SetFloat("y", currentY);
+        player.GetAnimator().SetFloat("x", currentX);
+        player.GetAnimator().SetFloat("y", currentY);
     }
 }
-
-
 
 public class CombatState : IPlayerState
 {
     private PlayerStateManager player;
-    private bool isAttacking;
+    private bool started;
 
     public void Enter(PlayerStateManager player)
     {
         this.player = player;
-        player.GetAnimator().SetTrigger("Attack");
-        player.GetCombatSystem().StartCombo();
-        isAttacking = true;
+        started = false;
     }
 
-    public void Exit()
+    public void Exit() {}
+
+    public void Update()
     {
-        player.GetAnimator().ResetTrigger("Attack");
-        isAttacking = false;
+        HandleInput();
+        if (!started)
+        {
+            player.GetCombatSystem().StartCombo();
+            started = true;
+        }
     }
-
-    public void Update() {}
 
     public void HandleInput()
     {
-        if (Input.GetButtonDown("Fire1") && !isAttacking)
+        if (Input.GetButtonDown("Fire1"))
         {
-            player.GetCombatSystem().LightAttack();
-            isAttacking = true;
+            player.GetCombatSystem().Attack("Fire1");
+            player.GetCombatSystem().HandleComboInput("Fire1");
         }
-        else if (Input.GetButtonDown("Fire2") && !isAttacking)
+        else if (Input.GetButtonDown("Fire2"))
         {
-            player.GetCombatSystem().HeavyAttack();
-            isAttacking = true;
+            player.GetCombatSystem().Attack("Fire2");
+            player.GetCombatSystem().HandleComboInput("Fire2");
         }
 
-        if (player.GetAnimator().GetCurrentAnimatorStateInfo(0).IsName("AttackFinish") && !isAttacking)
+        if (!Input.GetButton("Fire1") && !Input.GetButton("Fire2"))
         {
-            player.SetState(new IdleState());
+            AnimatorStateInfo st = player.GetAnimator().GetCurrentAnimatorStateInfo(0);
+            if (!st.loop && st.normalizedTime >= 1f)
+            {
+                player.SetState(new IdleState());
+            }
         }
     }
 }
