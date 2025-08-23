4ed850d4a9100e61992361638d60bd3ea10fbf1c
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-08-23 19:32:32 +0530

    Create Arrow.cs

---
+++ Assets/Scripts/Combat/Arrow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private TPVPlayerCombat.ArrowType arrowType;
+
+    public void SetArrowType(TPVPlayerCombat.ArrowType type)
+    {
+        arrowType = type;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var interactive = collision.collider.GetComponent<IArrowInteractive>();
+        if (interactive != null)
+        {
+            interactive.OnArrowHit(arrowType);
+        }
+
+        Destroy(gameObject);
+    }
+}
