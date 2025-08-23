0d11caf94a2c96c5d7370f61448db12b160028b2
WriothesleyWK <bm.sanjeetwk@gmail.com>
2025-08-23 16:59:33 +0530

    TPVController#2

--- Assets/Scripts/TPVPlayer/TPVPlayerMove.cs
+++ Assets/Scripts/TPVPlayer/TPVPlayerMove.cs
@@ -8,7 +8,7 @@ public class TPVPlayerMove : MonoBehaviour
     public float runSpeed = 8f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-    public Transform cam;
+    public Camera playerCam;
 
     [Header("Player GroundCheck")]
     public Transform groundCheck;
@@ -19,13 +19,43 @@ public class TPVPlayerMove : MonoBehaviour
     private bool isGrounded;
     private float gravity = -9.81f;
 
+    [Header("Player Animation")]
+    [SerializeField] private Animator animator;
+
+    [Header("Player Aim")]
+    [SerializeField] private Camera aimCam;
+    private bool isAiming;
+
+    private void Start()
+    {
+        aimCam.gameObject.SetActive(false);
+        playerCam.gameObject.SetActive(true);
+    }
+
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
-        ApplyGravity();
+        isAiming = Input.GetMouseButton(1);
+
+        if(isAiming && !isRunning)
+        {
+            aimCam.gameObject.SetActive(true);
+            playerCam.gameObject.SetActive(false);
+
+            animator.SetLayerWeight(1, 1f);
+        } else
+        {
+            aimCam.gameObject.SetActive(false);
+            playerCam.gameObject.SetActive(true);
+
+            animator.SetLayerWeight(1, 0f);
+        }
+
+
         HandleMovement(isRunning);
+        ApplyGravity();
 
         controller.Move(velocity * Time.deltaTime);
     }
@@ -51,7 +81,7 @@ public class TPVPlayerMove : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCam.transform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
@@ -59,6 +89,16 @@ public class TPVPlayerMove : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            if(isRunning)
+            {
+                animator.Play("StandardAnim_Running");
+            } else {
+                animator.Play("Walking");
+            }
+        } else
+        {
+            animator.Play("Player_Idle");
         }
     }
 
