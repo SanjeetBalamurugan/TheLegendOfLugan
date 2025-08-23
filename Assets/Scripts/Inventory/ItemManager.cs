42e926a3d6a1cdabf75a92bfaf9329d0cf06da2e
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-08-23 19:26:54 +0530

    Update ItemManager.cs

--- Assets/Scripts/Inventory/ItemManager.cs
+++ Assets/Scripts/Inventory/ItemManager.cs
@@ -12,12 +12,18 @@ public class ItemManager : MonoBehaviour
     public GameObject pickButtonPrefab;
     public List<Collider> picked;
 
+    [Header("Arrow Inventory")]
+    public int physicalArrowCount = 0;
+    public int pyroArrowCount = 0;
+    public int hydroArrowCount = 0;
+
     private void Awake()
     {
         instance = this;
     }
 
     public List<ItemScriptableObject> GetItemScriptableObjects() { return items; }
+
     public void AddInventoryItems(ItemScriptableObject item)
     {
         items.Add(item);
@@ -33,17 +39,54 @@ public class ItemManager : MonoBehaviour
     }
 
     public void AfterPickUp(List<Collider> colliders)
-{
-    for (var i = 0; i < pickButtons.childCount; i++)
     {
-        Transform buttonTransform = pickButtons.GetChild(i);
-        Collider buttonCollider = buttonTransform.GetComponent<Collider>();
-
-        if (buttonCollider != null && colliders.Contains(buttonCollider))
+        for (var i = 0; i < pickButtons.childCount; i++)
         {
-            Destroy(buttonTransform.gameObject);
+            Transform buttonTransform = pickButtons.GetChild(i);
+            Collider buttonCollider = buttonTransform.GetComponent<Collider>();
+
+            if (buttonCollider != null && colliders.Contains(buttonCollider))
+            {
+                Destroy(buttonTransform.gameObject);
+            }
         }
     }
-}
 
+    public int GetArrowCount(TPVPlayerCombat.ArrowType type)
+    {
+        switch (type)
+        {
+            case TPVPlayerCombat.ArrowType.Physical: return physicalArrowCount;
+            case TPVPlayerCombat.ArrowType.Pyro: return pyroArrowCount;
+            case TPVPlayerCombat.ArrowType.Hydro: return hydroArrowCount;
+            default: return 0;
+        }
+    }
+
+    public void AddArrows(TPVPlayerCombat.ArrowType type, int amount)
+    {
+        switch (type)
+        {
+            case TPVPlayerCombat.ArrowType.Physical: physicalArrowCount += amount; break;
+            case TPVPlayerCombat.ArrowType.Pyro: pyroArrowCount += amount; break;
+            case TPVPlayerCombat.ArrowType.Hydro: hydroArrowCount += amount; break;
+        }
+    }
+
+    public bool ConsumeArrow(TPVPlayerCombat.ArrowType type)
+    {
+        switch (type)
+        {
+            case TPVPlayerCombat.ArrowType.Physical:
+                if (physicalArrowCount > 0) { physicalArrowCount--; return true; }
+                break;
+            case TPVPlayerCombat.ArrowType.Pyro:
+                if (pyroArrowCount > 0) { pyroArrowCount--; return true; }
+                break;
+            case TPVPlayerCombat.ArrowType.Hydro:
+                if (hydroArrowCount > 0) { hydroArrowCount--; return true; }
+                break;
+        }
+        return false;
+    }
 }
