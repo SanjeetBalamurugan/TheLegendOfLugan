756a1e54932efc32615fcb0cee3863b9ec204b2a
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-08-23 19:27:32 +0530

    Update TPVPlayerCombat.cs

--- Assets/Scripts/Combat/TPVPlayerCombat.cs
+++ Assets/Scripts/Combat/TPVPlayerCombat.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 
 public class TPVPlayerCombat : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private BowWeapon bowWeapon;
-
-    [Header("Arrow Settings")]
-    public ArrowType currentArrowType = ArrowType.Physical;
-
     public enum ArrowType
     {
         Physical,
@@ -15,9 +9,45 @@ public class TPVPlayerCombat : MonoBehaviour
         Hydro
     }
 
-    void Update()
+    [Header("Arrow Settings")]
+    public ArrowType currentArrowType = ArrowType.Physical;
+
+    private void Update()
     {
-        if (bowWeapon != null)
-            bowWeapon.HandleCombat(currentArrowType);
+        HandleArrowSwitching();
+    }
+
+    private void HandleArrowSwitching()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentArrowType = ArrowType.Physical;
+            Debug.Log("Switched to Physical Arrow");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentArrowType = ArrowType.Pyro;
+            Debug.Log("Switched to Pyro Arrow");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentArrowType = ArrowType.Hydro;
+            Debug.Log("Switched to Hydro Arrow");
+        }
+    }
+
+    public bool HasArrows()
+    {
+        return ItemManager.instance.GetArrowCount(currentArrowType) > 0;
+    }
+
+    public bool TryConsumeArrow()
+    {
+        return ItemManager.instance.ConsumeArrow(currentArrowType);
+    }
+
+    public int GetCurrentArrowCount()
+    {
+        return ItemManager.instance.GetArrowCount(currentArrowType);
     }
 }
