6d04e91a2fd00c8cb5fbb5ddcf29b59d1968c0c2
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-09-08 18:26:44 +0530

    Create Core/SaveSystem.cs

---
+++ Assets/Scripts/Core/SaveSystem.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    private static string GetPath(SaveType type)
+    {
+        return Path.Combine(Application.persistentDataPath, type.ToString() + ".ldata");
+    }
+
+    public static void Save<T>(SaveType type, T data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream stream = new FileStream(GetPath(type), FileMode.Create))
+        {
+            formatter.Serialize(stream, data);
+        }
+        Debug.Log($"[{type}] Data saved at: {GetPath(type)}");
+    }
+
+    public static T Load<T>(SaveType type) where T : class
+    {
+        string path = GetPath(type);
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                return formatter.Deserialize(stream) as T;
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"[{type}] No save file found, returning null.");
+            return null;
+        }
+    }
+
+    public static void Delete(SaveType type)
+    {
+        string path = GetPath(type);
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            Debug.Log($"[{type}] Save file deleted.");
+        }
+    }
+}
