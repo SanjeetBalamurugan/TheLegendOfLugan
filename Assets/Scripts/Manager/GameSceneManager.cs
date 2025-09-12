70ff93d308792e68b199f78d993224b9ca1d3297
WriothesleyWK <bm.sanjeetwk@gmail.com>
2025-09-12 09:45:21 +0530

    Revert "u"
    This reverts commit 57874253a006cddc077f9923e6cd8e38fd3cbc9d.

--- Assets/Scripts/Manager/GameSceneManager.cs
+++ Assets/Scripts/Manager/GameSceneManager.cs
@@ -50,7 +50,6 @@ public class GameSceneManager : MonoBehaviour
 
     private IEnumerator LoadNextSceneAsync(GameScene scene)
     {
-<<<<<<< HEAD
         if (SceneManager.GetSceneByName(currentScene.ToString()).isLoaded)
         {
             AsyncOperation unloadOp = SceneManager.UnloadSceneAsync(currentScene.ToString());
@@ -64,9 +63,6 @@ public class GameSceneManager : MonoBehaviour
         if (LoadingScreenManager.Instance != null)
             LoadingScreenManager.Instance.FadeIn();
 
-=======
-        SceneManager.UnloadSceneAsync(currentScene.ToString());
->>>>>>> parent of e4b46c7 (Update GameSceneManager.cs)
         AsyncOperation loadOp = SceneManager.LoadSceneAsync(scene.ToString(), LoadSceneMode.Additive);
         loadOp.allowSceneActivation = false;
 
@@ -86,15 +82,12 @@ public class GameSceneManager : MonoBehaviour
             yield return null;
 
         SceneManager.SetActiveScene(SceneManager.GetSceneByName(scene.ToString()));
-<<<<<<< HEAD
         currentScene = scene;
 
         yield return null;
 
         if (LoadingScreenManager.Instance != null)
             LoadingScreenManager.Instance.FadeOut();
-=======
->>>>>>> parent of e4b46c7 (Update GameSceneManager.cs)
 
         OnSceneLoaded?.Invoke(scene);
     }
