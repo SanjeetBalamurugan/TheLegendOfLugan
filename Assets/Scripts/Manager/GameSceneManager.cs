134c37e10683051329dd919488e380f1b1a3644d
WriothesleyWK <bm.sanjeetwk@gmail.com>
2025-09-12 09:58:28 +0530

    Update GameSceneManager.cs

--- Assets/Scripts/Manager/GameSceneManager.cs
+++ Assets/Scripts/Manager/GameSceneManager.cs
@@ -21,31 +21,25 @@ public class GameSceneManager : MonoBehaviour
 
     private void Start()
     {
+        // Load settings automatically at startup
         LoadSettingsData();
     }
 
     private IEnumerator LoadGameSequence()
     {
         if (!SceneManager.GetSceneByName(GameScene.Persistent.ToString()).isLoaded)
-        {
-            if (Application.CanStreamedLevelBeLoaded(GameScene.Persistent.ToString()))
-                yield return SceneManager.LoadSceneAsync(GameScene.Persistent.ToString(), LoadSceneMode.Additive);
-            else
-                yield break;
-        }
+            yield return SceneManager.LoadSceneAsync(GameScene.Persistent.ToString(), LoadSceneMode.Additive);
 
         if (useLoadingScreen)
         {
-            if (Application.CanStreamedLevelBeLoaded(GameScene.LoadingScreen.ToString()))
-                yield return SceneManager.LoadSceneAsync(GameScene.LoadingScreen.ToString(), LoadSceneMode.Additive);
-            else
-                yield break;
-        }
-
-        yield return StartCoroutine(LoadNextSceneAsync(nextSceneToLoad));
-
-        if (useLoadingScreen && SceneManager.GetSceneByName(GameScene.LoadingScreen.ToString()).isLoaded)
+            yield return SceneManager.LoadSceneAsync(GameScene.LoadingScreen.ToString(), LoadSceneMode.Additive);
+            yield return StartCoroutine(LoadNextSceneAsync(nextSceneToLoad));
             SceneManager.UnloadSceneAsync(GameScene.LoadingScreen.ToString());
+        }
+        else
+        {
+            yield return StartCoroutine(LoadNextSceneAsync(nextSceneToLoad));
+        }
     }
 
     private IEnumerator LoadNextSceneAsync(GameScene scene)
@@ -63,6 +57,7 @@ public class GameSceneManager : MonoBehaviour
         if (LoadingScreenManager.Instance != null)
             LoadingScreenManager.Instance.FadeIn();
 
+        SceneManager.UnloadSceneAsync(currentScene.ToString());
         AsyncOperation loadOp = SceneManager.LoadSceneAsync(scene.ToString(), LoadSceneMode.Additive);
         loadOp.allowSceneActivation = false;
 
@@ -101,12 +96,13 @@ public class GameSceneManager : MonoBehaviour
 
     public void ReloadCurrentScene()
     {
-        GameScene scene = (GameScene)System.Enum.Parse(typeof(GameScene), SceneManager.GetActiveScene().name);
-        LoadScene(scene, useLoadingScreen);
+        GameScene currentScene = (GameScene)System.Enum.Parse(typeof(GameScene), SceneManager.GetActiveScene().name);
+        LoadScene(currentScene, useLoadingScreen);
     }
 
     public void QuitToMainMenu()
     {
+        // Save settings before quitting
         SaveSettingsData();
         LoadScene(GameScene.MainMenu, true);
     }
@@ -116,8 +112,10 @@ public class GameSceneManager : MonoBehaviour
         return (GameScene)System.Enum.Parse(typeof(GameScene), SceneManager.GetActiveScene().name);
     }
 
+    // --- SAVE/LOAD HOOKS ---
     private void SaveSettingsData()
     {
+        // Example: pull data from AudioManager
         float bgm = PlayerPrefs.GetFloat("BGMVol", 1f);
         float ui = PlayerPrefs.GetFloat("UIVol", 1f);
         int quality = QualitySettings.GetQualityLevel();
@@ -131,8 +129,11 @@ public class GameSceneManager : MonoBehaviour
         SettingsSaveData data = SaveSystem.Load<SettingsSaveData>(SaveType.Settings);
         if (data != null)
         {
+            // Apply audio
             AudioManager.Instance.SetBGMVolume(data.bgmVolume);
             AudioManager.Instance.SetUIVolume(data.uiVolume);
+
+            // Apply graphics
             QualitySettings.SetQualityLevel(data.qualityPreset, true);
         }
     }
